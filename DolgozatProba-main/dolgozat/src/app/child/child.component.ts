d244a718f37ae907d843b84a446950bb7b6bd689
ttamas0201 <123103523+ttamas0201@users.noreply.github.com>
2025-02-10 15:54:48 +0100

    Add files via upload

---
+++ DolgozatProba-main/dolgozat/src/app/child/child.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  standalone: false,
+  templateUrl: './child.component.html',
+  styleUrl: './child.component.css'
+})
+export class ChildComponent {
+
+}
